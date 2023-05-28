291d2060ac0f1525790cd061dceb5744da883256
MOUFTAOU Hanane <mouftaouhanane@gmail.com>
2023-05-28 10:27:00 +0100

    ajout des validators pour la creation dune formation

--- api_glewe/api_glewe/app/Http/Controllers/Api/CourseController.php
+++ api_glewe/api_glewe/app/Http/Controllers/Api/CourseController.php
@@ -7,5 +7,59 @@ use Illuminate\Http\Request;
 
 class CourseController extends Controller
 {
-    //
+    protected OperationService $_operationService;
+    public function __construct(OperationService $operationService)
+    {
+       $this->_operationService = $operationService;
+    }
+
+
+     //listing marque
+     public function createCourse(Request $request){
+        try{
+            $rData=$request->only(['name', "category_id", 'trainer_name', 'price','description', "course_type", "course_level","estimated_duration","language_id"]);
+            $validator=[
+                'name' => ['required'],
+                'category_id' => ['required','exists:categories,id'],
+                'trainer_name' => ['required'],
+                'price' => ['required'],
+                'description' => ['required'],
+                'course_type' => ['required'],
+                'course_level' => ['required'],
+                'estimated_duration' => ['required'],
+                'language_id' => ['required', 'exists:languages,id']
+            ];
+            $validationMessages = [
+                'name.required' => "Le nom de la formation est requis",
+                'category_id.required' => "La catégorie de la formation est requise",
+                'category_id.exists' => "La catégorie de la formation n'est pas valide",
+                'trainer_name.required' => "Le nom du formateur est requis",
+                'price.required' => "Le prix de la formation est requis est requis",
+                'description.required' => "Une description de la formation est requise",
+                'course_type.required' => "Le type de la formation est requise",
+                'course_level.required' => "Le niveau de la formation est requis",
+                'estimated_duration.required' => "La durée estimée par le cours est requise",
+                'language_id.required' => "Le language de la formation est requis",
+                'language_id.exists' => "Le language de la formation n'est pas valide",
+
+
+            ];
+            $validatorResult=Validator::make( $rData, $validator, $validationMessages);
+
+            if ($validatorResult->fails()) {
+                return response()->json([
+                    'data' => $validatorResult->errors()->first(),
+                    'status' => "error",
+                    'message' => $validatorResult->errors()->first(),
+                ], 400);
+            }
+
+
+
+
+        }catch(Exception $ex){
+            throw new Exception($ex);
+        }
+
+    }
 }
